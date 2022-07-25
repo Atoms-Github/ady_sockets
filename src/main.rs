ec9d5cecac3c59d6162b38a4596699562ad607bc
Thomas Mullender <tomullender@gmail.com>
2022-07-25 20:02:42 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+#![allow(unused_mut)]
+fn main() {
+    println!("Hello, world!");
+    let e = 2;
+    let b = e + 2;
+    let tree = e;
+    
+}
